2ccd954d3b3d2e1b1848e3234b333da8836a95e9
David <david.v.bernardo@gmail.com>
2022-09-21 12:50:59 -0500

    commit tree 53a0b42581d9d63c6004f2adef7e57b6d3cc25a4

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const mid = Math.floor(word.length / 2);
+  const last = word.length - 1;
+  for (let i = 0; i < mid; ++i)
+  {
+    if(word[i] === word[last - i])
+      continue;
+    return false;
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  for i = 0...(len / 2)
+    if str[i] === str[len - 1 - i]
+     continue
+    return false
+  return true
 */
 
 /*
   Add written explanation of your solution here
+  for a string of length 6, compare
+   s[0] to s[5]
+   s[1] to s[4]
+   s[2] to s[3]
+   break at once if there's a mismatch
 */
 
 // You can run `node index.js` to view these console logs
